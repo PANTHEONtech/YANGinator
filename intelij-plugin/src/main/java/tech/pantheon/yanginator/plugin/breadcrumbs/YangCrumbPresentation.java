3c13044294338a1b602bc191eb7c70d1e498bb66
Samuel Schneider <samuel.schneider@pantheon.tech>
2021-12-03 15:33:14 +0100

    Updated copyright
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/breadcrumbs/YangCrumbPresentation.java
+++ intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/breadcrumbs/YangCrumbPresentation.java
@@ -1,9 +1,18 @@
+/*
+ * Copyright (c) 2021 PANTHEON.tech, s.r.o. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
 package tech.pantheon.yanginator.plugin.breadcrumbs;
 
 import com.intellij.ui.JBColor;
 import com.intellij.xml.breadcrumbs.CrumbPresentation;
 
-import java.awt.Color;
+import java.awt.*;
 
 public class YangCrumbPresentation extends CrumbPresentation {
     private final JBColor color;
