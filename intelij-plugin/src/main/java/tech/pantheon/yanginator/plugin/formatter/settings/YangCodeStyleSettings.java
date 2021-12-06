00fe7562ceb2a56dde771554dcd943074dacafe3
Samuel Schneider <samuel.schneider@pantheon.tech>
2021-12-06 16:04:54 +0100

    Added rules for spacing
    Signed-off-by: Samuel Schneider <samuel.schneider@pantheon.tech>

--- intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/formatter/settings/YangCodeStyleSettings.java
+++ intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/formatter/settings/YangCodeStyleSettings.java
@@ -14,7 +14,7 @@ import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
 
 public class YangCodeStyleSettings extends CustomCodeStyleSettings {
 
-    public boolean spacesAfterKeyword = true;
+    public boolean spacesBeforeLeftBrace = true;
 
     public YangCodeStyleSettings(final CodeStyleSettings settings) {
         super("YangCodeStyleSettings", settings);
