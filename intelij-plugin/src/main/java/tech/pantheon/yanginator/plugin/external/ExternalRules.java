0d6cfaab8d846ee546c9f7639278df135a0fbdce
tobias.pobocik <tobias.pobocik@pantheon.tech>
2022-03-21 10:23:50 +0100

    Add AnyOrder external rule
    -rename variables to be even more descriptive.
    
    Signed-off-by: tobias.pobocik <tobias.pobocik@pantheon.tech>

--- intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/external/ExternalRules.java
+++ intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/external/ExternalRules.java
@@ -15,59 +15,59 @@ public class ExternalRules {
 
     /**
      * First loop will go from first statement(word) to last, second loop will try to match every single item
-     * from listOfItems with the statement,
+     * from parsers with the statement,
      * if the item matches but offset does not change that means that the parsed word was optional. if we match optional word
      * we save it to HashSet and try parsing it later. if it does match and offset moves that mean that it was parsed, so we
      * can remove it.
      * see more in /yanginator/plugin/external/AnyOrderRuleTutorial.md
      *
-     * @param psiBuilder  Psi builder
-     * @param level       Level, by increasing it we move to the next element
-     * @param listOfItems list of items to try parsing and matching
+     * @param psiBuilder Psi builder
+     * @param level      Level, by increasing it we move to the next element
+     * @param parsers    list of items to try parsing and matching
      * @return return true if all items are matched BUT does not check for duplicates
      */
 
-    public static boolean anyOrder(PsiBuilder psiBuilder, int level, Parser... listOfItems) {
+    public static boolean anyOrder(PsiBuilder psiBuilder, int level, Parser... parsers) {
         if (!recursion_guard_(psiBuilder, level, "rule")) return false;
-        boolean parserResultBoolean;
+        boolean result;
         //create new array of booleans with length of items that we want to match
-        boolean[] listOfBooleanResults = new boolean[listOfItems.length];
-        //create list out of listOfItems array, so we can remove items that were already matched
-        List<Parser> listOfItemsToParse = new ArrayList<>(List.of(listOfItems));
-        Set<Parser> optionalParsed = new HashSet<>();
+        boolean[] individualParserResults = new boolean[parsers.length];
+        //create list out of parsers array, so we can remove items that were already matched
+        List<Parser> parsersHolder = new ArrayList<>(List.of(parsers));
+        Set<Parser> optionalParserHolder = new HashSet<>();
         //enter the section to match
         PsiBuilder.Marker marker = enter_section_(psiBuilder);
 
-        for (int i = 0; i < listOfItems.length; i++) {
-            parserResultBoolean = false;
-            for (int j = 0; j < listOfItemsToParse.size(); j++) {
+        for (int i = 0; i < parsers.length; i++) {
+            result = false;
+            for (int j = 0; j < parsersHolder.size(); j++) {
                 int oldOffset = psiBuilder.getCurrentOffset();
                 //try parsing item
-                parserResultBoolean = listOfItemsToParse.get(j).parse(psiBuilder, level + 1);
-                if (i == j && parserResultBoolean) {
-                    listOfBooleanResults[i] = true;
+                result = parsersHolder.get(j).parse(psiBuilder, level + 1);
+                if (i == j && result) {
+                    individualParserResults[i] = true;
                 }
                 int newOffset = psiBuilder.getCurrentOffset();
                 //optional or not optional
-                if (parserResultBoolean && (oldOffset == newOffset)) {
-                    optionalParsed.add(listOfItems[j]);
+                if (result && (oldOffset == newOffset)) {
+                    optionalParserHolder.add(parsers[j]);
                     //try parsing optional
-                    for (Parser item : optionalParsed) {
+                    for (Parser item : optionalParserHolder) {
                         oldOffset = psiBuilder.getCurrentOffset();
-                        parserResultBoolean = item.parse(psiBuilder, level + 1);
-                        if (parserResultBoolean && oldOffset != newOffset) {
+                        result = item.parse(psiBuilder, level + 1);
+                        if (result && oldOffset != newOffset) {
                             //remove optional item if it was parsed
-                            optionalParsed.remove(item);
-                            listOfBooleanResults[i] = true;
+                            optionalParserHolder.remove(item);
+                            individualParserResults[i] = true;
                             //no need to loop futher, the item has been parsed
                             break;
                         } else {
-                            parserResultBoolean = false;
+                            result = false;
                         }
                     }
                 }
-                if (parserResultBoolean) {
-                    listOfBooleanResults[j] = true;
+                if (result) {
+                    individualParserResults[j] = true;
                     //no need to loop futher, the item has been parsed
                     break;
                 }
@@ -75,14 +75,15 @@ public class ExternalRules {
             //move to next element and try matching it
             level += 1;
         }
-        exit_section_(psiBuilder, marker, null, areAllTrue(listOfBooleanResults));
-        return areAllTrue(listOfBooleanResults);
+        exit_section_(psiBuilder, marker, null, unanimousResults(individualParserResults));
+        return unanimousResults(individualParserResults);
     }
 
-    private static boolean areAllTrue(boolean[] listOfBooleanResults) {
+    private static boolean unanimousResults(boolean[] listOfBooleanResults) {
         //simple method to find if all elements are true
-        for (boolean booleanList : listOfBooleanResults) if (!booleanList) return false;
+        for (boolean booleanList : listOfBooleanResults) {
+            if (!booleanList) return false;
+        }
         return true;
     }
-
 }
