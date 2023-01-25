335fc1c850baf7bfec03e937796c825d221fd040
filip.vitkovsky <filip.vitkovsky@pantheon.tech>
2023-01-25 17:28:55 +0100

    Changed String regex for highlighting
    Signed-off-by: filip.vitkovsky <filip.vitkovsky@pantheon.tech>

--- intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangDummy.java
+++ intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangDummy.java
@@ -1,32 +1,20 @@
-/*
- *
- *   Copyright (c) 2021-2022 PANTHEON.tech, s.r.o. All rights reserved.
- *
- *   This program and the accompanying materials are made available under the
- *   terms of the Eclipse Public License v1.0 which accompanies this distribution,
- *   and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- */
-
 // This is a generated file. Not intended for manual editing.
 package tech.pantheon.yanginator.plugin.highlighter.psi;
 
-import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import tech.pantheon.yanginator.plugin.psi.YangNamedElement;
-
 import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+import tech.pantheon.yanginator.plugin.psi.YangNamedElement;
 
 public interface YangDummy extends YangNamedElement {
 
-    @NotNull
-    List<YangWsp> getWspList();
+  @NotNull
+  List<YangWsp> getWspList();
 
-    @NotNull
-    PsiElement getHIdentifier();
+  @NotNull
+  PsiElement getHIdentifier();
 
-    @Nullable
-    PsiElement getHString();
+  @Nullable
+  PsiElement getHString();
 
 }
--- intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangHIdentifierLiteral.java
+++ intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangHIdentifierLiteral.java
@@ -1,305 +1,296 @@
-/*
- *
- *   Copyright (c) 2021-2022 PANTHEON.tech, s.r.o. All rights reserved.
- *
- *   This program and the accompanying materials are made available under the
- *   terms of the Eclipse Public License v1.0 which accompanies this distribution,
- *   and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- */
-
 // This is a generated file. Not intended for manual editing.
 package tech.pantheon.yanginator.plugin.highlighter.psi;
 
+import java.util.List;
+import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.Nullable;
 import tech.pantheon.yanginator.plugin.psi.YangNamedElement;
 
 public interface YangHIdentifierLiteral extends YangNamedElement {
 
-    @Nullable
-    PsiElement getHActionKeyword();
+  @Nullable
+  PsiElement getHActionKeyword();
 
-    @Nullable
-    PsiElement getHAddKeyword();
+  @Nullable
+  PsiElement getHAddKeyword();
 
-    @Nullable
-    PsiElement getHAndKeyword();
+  @Nullable
+  PsiElement getHAndKeyword();
 
-    @Nullable
-    PsiElement getHAnydataKeyword();
+  @Nullable
+  PsiElement getHAnydataKeyword();
 
-    @Nullable
-    PsiElement getHAnyxmlKeyword();
+  @Nullable
+  PsiElement getHAnyxmlKeyword();
 
-    @Nullable
-    PsiElement getHArgumentKeyword();
+  @Nullable
+  PsiElement getHArgumentKeyword();
 
-    @Nullable
-    PsiElement getHAugmentKeyword();
+  @Nullable
+  PsiElement getHAugmentKeyword();
 
-    @Nullable
-    PsiElement getHBaseKeyword();
+  @Nullable
+  PsiElement getHBaseKeyword();
 
-    @Nullable
-    PsiElement getHBelongsToKeyword();
+  @Nullable
+  PsiElement getHBelongsToKeyword();
 
-    @Nullable
-    PsiElement getHBitKeyword();
+  @Nullable
+  PsiElement getHBitKeyword();
 
-    @Nullable
-    PsiElement getHCaseKeyword();
+  @Nullable
+  PsiElement getHCaseKeyword();
 
-    @Nullable
-    PsiElement getHChoiceKeyword();
+  @Nullable
+  PsiElement getHChoiceKeyword();
 
-    @Nullable
-    PsiElement getHConfigKeyword();
+  @Nullable
+  PsiElement getHConfigKeyword();
 
-    @Nullable
-    PsiElement getHContactKeyword();
+  @Nullable
+  PsiElement getHContactKeyword();
 
-    @Nullable
-    PsiElement getHContainerKeyword();
+  @Nullable
+  PsiElement getHContainerKeyword();
 
-    @Nullable
-    PsiElement getHCurrentKeyword();
+  @Nullable
+  PsiElement getHCurrentKeyword();
 
-    @Nullable
-    PsiElement getHDefaultKeyword();
+  @Nullable
+  PsiElement getHDefaultKeyword();
 
-    @Nullable
-    PsiElement getHDeleteKeyword();
+  @Nullable
+  PsiElement getHDeleteKeyword();
 
-    @Nullable
-    PsiElement getHDeprecatedKeyword();
+  @Nullable
+  PsiElement getHDeprecatedKeyword();
 
-    @Nullable
-    PsiElement getHDescriptionKeyword();
+  @Nullable
+  PsiElement getHDescriptionKeyword();
 
-    @Nullable
-    PsiElement getHDeviateKeyword();
+  @Nullable
+  PsiElement getHDeviateKeyword();
 
-    @Nullable
-    PsiElement getHDeviationKeyword();
+  @Nullable
+  PsiElement getHDeviationKeyword();
 
-    @Nullable
-    PsiElement getHDigit();
+  @Nullable
+  PsiElement getHDigit();
 
-    @Nullable
-    PsiElement getHEnumKeyword();
+  @Nullable
+  PsiElement getHEnumKeyword();
 
-    @Nullable
-    PsiElement getHErrorAppTagKeyword();
+  @Nullable
+  PsiElement getHErrorAppTagKeyword();
 
-    @Nullable
-    PsiElement getHErrorMessageKeyword();
+  @Nullable
+  PsiElement getHErrorMessageKeyword();
 
-    @Nullable
-    PsiElement getHExtensionKeyword();
+  @Nullable
+  PsiElement getHExtensionKeyword();
 
-    @Nullable
-    PsiElement getHFalseKeyword();
+  @Nullable
+  PsiElement getHFalseKeyword();
 
-    @Nullable
-    PsiElement getHFeatureKeyword();
+  @Nullable
+  PsiElement getHFeatureKeyword();
 
-    @Nullable
-    PsiElement getHFractionDigitsKeyword();
+  @Nullable
+  PsiElement getHFractionDigitsKeyword();
 
-    @Nullable
-    PsiElement getHGroupingKeyword();
+  @Nullable
+  PsiElement getHGroupingKeyword();
 
-    @Nullable
-    PsiElement getHIdentityKeyword();
+  @Nullable
+  PsiElement getHIdentityKeyword();
 
-    @Nullable
-    PsiElement getHIfFeatureKeyword();
+  @Nullable
+  PsiElement getHIfFeatureKeyword();
 
-    @Nullable
-    PsiElement getHImportKeyword();
+  @Nullable
+  PsiElement getHImportKeyword();
 
-    @Nullable
-    PsiElement getHIncludeKeyword();
+  @Nullable
+  PsiElement getHIncludeKeyword();
 
-    @Nullable
-    PsiElement getHInputKeyword();
+  @Nullable
+  PsiElement getHInputKeyword();
 
-    @Nullable
-    PsiElement getHInvalidEscape();
+  @Nullable
+  PsiElement getHInvalidEscape();
 
-    @Nullable
-    PsiElement getHInvertMatchKeyword();
+  @Nullable
+  PsiElement getHInvertMatchKeyword();
 
-    @Nullable
-    PsiElement getHKeywordMatcher();
+  @Nullable
+  PsiElement getHKeywordMatcher();
 
-    @Nullable
-    PsiElement getHKeyKeyword();
+  @Nullable
+  PsiElement getHKeyKeyword();
 
-    @Nullable
-    PsiElement getHLeafKeyword();
+  @Nullable
+  PsiElement getHLeafKeyword();
 
-    @Nullable
-    PsiElement getHLeafListKeyword();
+  @Nullable
+  PsiElement getHLeafListKeyword();
 
-    @Nullable
-    PsiElement getHLeftBrace();
+  @Nullable
+  PsiElement getHLeftBrace();
 
-    @Nullable
-    PsiElement getHLengthKeyword();
+  @Nullable
+  PsiElement getHLengthKeyword();
 
-    @Nullable
-    PsiElement getHListKeyword();
+  @Nullable
+  PsiElement getHListKeyword();
 
-    @Nullable
-    PsiElement getHMandatoryKeyword();
+  @Nullable
+  PsiElement getHMandatoryKeyword();
 
-    @Nullable
-    PsiElement getHMaxElementsKeyword();
+  @Nullable
+  PsiElement getHMaxElementsKeyword();
 
-    @Nullable
-    PsiElement getHMaxKeyword();
+  @Nullable
+  PsiElement getHMaxKeyword();
 
-    @Nullable
-    PsiElement getHMinElementsKeyword();
+  @Nullable
+  PsiElement getHMinElementsKeyword();
 
-    @Nullable
-    PsiElement getHMinKeyword();
+  @Nullable
+  PsiElement getHMinKeyword();
 
-    @Nullable
-    PsiElement getHModifierKeyword();
+  @Nullable
+  PsiElement getHModifierKeyword();
 
-    @Nullable
-    PsiElement getHModuleKeyword();
+  @Nullable
+  PsiElement getHModuleKeyword();
 
-    @Nullable
-    PsiElement getHMustKeyword();
+  @Nullable
+  PsiElement getHMustKeyword();
 
-    @Nullable
-    PsiElement getHNamespaceKeyword();
+  @Nullable
+  PsiElement getHNamespaceKeyword();
 
-    @Nullable
-    PsiElement getHNonWord();
+  @Nullable
+  PsiElement getHNonWord();
 
-    @Nullable
-    PsiElement getHNotificationKeyword();
+  @Nullable
+  PsiElement getHNotificationKeyword();
 
-    @Nullable
-    PsiElement getHNotKeyword();
+  @Nullable
+  PsiElement getHNotKeyword();
 
-    @Nullable
-    PsiElement getHNotSupportedKeyword();
+  @Nullable
+  PsiElement getHNotSupportedKeyword();
 
-    @Nullable
-    PsiElement getHObsoleteKeyword();
+  @Nullable
+  PsiElement getHObsoleteKeyword();
 
-    @Nullable
-    PsiElement getHOperators();
+  @Nullable
+  PsiElement getHOperators();
 
-    @Nullable
-    PsiElement getHOrderedByKeyword();
+  @Nullable
+  PsiElement getHOrderedByKeyword();
 
-    @Nullable
-    PsiElement getHOrganizationKeyword();
+  @Nullable
+  PsiElement getHOrganizationKeyword();
 
-    @Nullable
-    PsiElement getHOrKeyword();
+  @Nullable
+  PsiElement getHOrKeyword();
 
-    @Nullable
-    PsiElement getHOutputKeyword();
+  @Nullable
+  PsiElement getHOutputKeyword();
 
-    @Nullable
-    PsiElement getHPathKeyword();
+  @Nullable
+  PsiElement getHPathKeyword();
 
-    @Nullable
-    PsiElement getHPatternKeyword();
+  @Nullable
+  PsiElement getHPatternKeyword();
 
-    @Nullable
-    PsiElement getHPositionKeyword();
+  @Nullable
+  PsiElement getHPositionKeyword();
 
-    @Nullable
-    PsiElement getHPrefixKeyword();
+  @Nullable
+  PsiElement getHPrefixKeyword();
 
-    @Nullable
-    PsiElement getHPresenceKeyword();
+  @Nullable
+  PsiElement getHPresenceKeyword();
 
-    @Nullable
-    PsiElement getHRangeKeyword();
+  @Nullable
+  PsiElement getHRangeKeyword();
 
-    @Nullable
-    PsiElement getHReferenceKeyword();
+  @Nullable
+  PsiElement getHReferenceKeyword();
 
-    @Nullable
-    PsiElement getHRefineKeyword();
+  @Nullable
+  PsiElement getHRefineKeyword();
 
-    @Nullable
-    PsiElement getHReplaceKeyword();
+  @Nullable
+  PsiElement getHReplaceKeyword();
 
-    @Nullable
-    PsiElement getHRequireInstanceKeyword();
+  @Nullable
+  PsiElement getHRequireInstanceKeyword();
 
-    @Nullable
-    PsiElement getHRevisionDateKeyword();
+  @Nullable
+  PsiElement getHRevisionDateKeyword();
 
-    @Nullable
-    PsiElement getHRevisionKeyword();
+  @Nullable
+  PsiElement getHRevisionKeyword();
 
-    @Nullable
-    PsiElement getHRightBrace();
+  @Nullable
+  PsiElement getHRightBrace();
 
-    @Nullable
-    PsiElement getHRpcKeyword();
+  @Nullable
+  PsiElement getHRpcKeyword();
 
-    @Nullable
-    PsiElement getHSemicolon();
+  @Nullable
+  PsiElement getHSemicolon();
 
-    @Nullable
-    PsiElement getHStatusKeyword();
+  @Nullable
+  PsiElement getHStatusKeyword();
 
-    @Nullable
-    PsiElement getHSubmoduleKeyword();
+  @Nullable
+  PsiElement getHSubmoduleKeyword();
 
-    @Nullable
-    PsiElement getHSystemKeyword();
+  @Nullable
+  PsiElement getHSystemKeyword();
 
-    @Nullable
-    PsiElement getHTrueKeyword();
+  @Nullable
+  PsiElement getHTrueKeyword();
 
-    @Nullable
-    PsiElement getHTypedefKeyword();
+  @Nullable
+  PsiElement getHTypedefKeyword();
 
-    @Nullable
-    PsiElement getHTypeKeyword();
+  @Nullable
+  PsiElement getHTypeKeyword();
 
-    @Nullable
-    PsiElement getHUnboundedKeyword();
+  @Nullable
+  PsiElement getHUnboundedKeyword();
 
-    @Nullable
-    PsiElement getHUniqueKeyword();
+  @Nullable
+  PsiElement getHUniqueKeyword();
 
-    @Nullable
-    PsiElement getHUnitsKeyword();
+  @Nullable
+  PsiElement getHUnitsKeyword();
 
-    @Nullable
-    PsiElement getHUserKeyword();
+  @Nullable
+  PsiElement getHUserKeyword();
 
-    @Nullable
-    PsiElement getHUsesKeyword();
+  @Nullable
+  PsiElement getHUsesKeyword();
 
-    @Nullable
-    PsiElement getHValidEscape();
+  @Nullable
+  PsiElement getHValidEscape();
 
-    @Nullable
-    PsiElement getHValueKeyword();
+  @Nullable
+  PsiElement getHValueKeyword();
 
-    @Nullable
-    PsiElement getHWhenKeyword();
+  @Nullable
+  PsiElement getHWhenKeyword();
 
-    @Nullable
-    PsiElement getHYangVersionKeyword();
+  @Nullable
+  PsiElement getHYangVersionKeyword();
 
-    @Nullable
-    PsiElement getHYinElementKeyword();
+  @Nullable
+  PsiElement getHYinElementKeyword();
 
 }
