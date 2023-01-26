a89b7b7bc144f8df3016b95401c5ea08801b7246
filip.vitkovsky <filip.vitkovsky@pantheon.tech>
2023-01-26 12:13:05 +0100

    Changed String regex for highlighting
    Signed-off-by: filip.vitkovsky <filip.vitkovsky@pantheon.tech>

--- intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangStmt.java
+++ intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/highlighter/psi/YangStmt.java
@@ -1,23 +1,34 @@
+/*
+ *
+ *   Copyright (c) 2021-2023 PANTHEON.tech, s.r.o. All rights reserved.
+ *
+ *   This program and the accompanying materials are made available under the
+ *   terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *   and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ */
+
 // This is a generated file. Not intended for manual editing.
 package tech.pantheon.yanginator.plugin.highlighter.psi;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
 import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
 import tech.pantheon.yanginator.plugin.psi.YangNamedElement;
+
+import java.util.List;
 
 public interface YangStmt extends YangNamedElement {
 
-  @NotNull
-  List<YangWsp> getWspList();
+    @NotNull
+    List<YangWsp> getWspList();
 
-  @NotNull
-  PsiElement getHIdentifier();
+    @NotNull
+    PsiElement getHIdentifier();
 
-  @NotNull
-  PsiElement getHSemicolon();
+    @NotNull
+    PsiElement getHSemicolon();
 
-  @NotNull
-  PsiElement getHString();
+    @NotNull
+    PsiElement getHString();
 
 }
