9ea0125a6aad01ab666b81ed65a52e6f203bddd9
peterivanic <60007423+peterivanic@users.noreply.github.com>
2023-02-10 14:21:30 +0100

    fix index out of bounds in empty yang (#177)
    Signed-off-by: peter.ivanic <peter.ivanic@pantheon.tech>

--- rfc-parser/src/main/java/tech/pantheon/yanginator/parser/services/GrammarKitRFCUncomplaintUtils.java
+++ rfc-parser/src/main/java/tech/pantheon/yanginator/parser/services/GrammarKitRFCUncomplaintUtils.java
@@ -531,6 +531,7 @@ public class GrammarKitRFCUncomplaintUtils {
         List<String> result = new ArrayList<>();
         for (String line : lines) {
             if (line.contains("yang ::=  (module-stmt | submodule-stmt)")) {
+                line = "yang ::=  (module-stmt | submodule-stmt | ( WSP | ZERO_LENGTH_STRING | LINEFEED | CARRIAGE_RETURN )*)";
                 result.add(line);
                 result.add("");
                 result.add("private tokens ::= BLOCK_COMMENT | ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN |");
