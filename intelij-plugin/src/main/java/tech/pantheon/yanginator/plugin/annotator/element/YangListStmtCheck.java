50cec4e175b5e2cc1210b71a93e1437a617b1c16
Matej.Sramcik <matej.sramcik@pantheon.tech>
2023-03-30 14:36:23 +0200

    list-stmt anotator fix for data-def-stmt
    Signed-off-by: Matej.Sramcik <matej.sramcik@pantheon.tech>

--- intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/annotator/element/YangListStmtCheck.java
+++ intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/annotator/element/YangListStmtCheck.java
@@ -13,7 +13,6 @@ package tech.pantheon.yanginator.plugin.annotator.element;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.psi.PsiElement;
 import org.jetbrains.annotations.NotNull;
-import tech.pantheon.yanginator.plugin.annotator.check.ElementCheckUtils;
 import tech.pantheon.yanginator.plugin.psi.YangConfigStmt;
 import tech.pantheon.yanginator.plugin.psi.YangDescriptionStmt;
 import tech.pantheon.yanginator.plugin.psi.YangKeyStmt;
@@ -42,7 +41,6 @@ public class YangListStmtCheck extends AbstractYangStmtCheck {
         maxOne.check(element, holder, YangStatusStmt.class);
         maxOne.check(element, holder, YangDescriptionStmt.class);
         maxOne.check(element, holder, YangReferenceStmt.class);
-        minOne.checkMany(element, holder, ElementCheckUtils.getDataDefStatements());
         for (PsiElement child : element.getChildren()) {
             if (child instanceof YangConfigStmt && ((YangConfigStmt) child).getConfigArgStr() != null) {
                 String configArg = ((YangConfigStmt) child).getConfigArgStr().getText().replaceAll("\"", "");
