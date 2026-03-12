e593874153e445f902bbd61bbc68ae87e0eb589f
Lubos Janoscik <lubos.janoscik@pantheon.tech>
2026-03-12 13:00:23 +0000

    Updating copyright (#322)
    Signed-off-by: Janoscik, Lubos <lubos.janoscik@pantheon.tech>
    Signed-off-by: Lubos Janoscik <lubos.janoscik@pantheon.tech>

--- intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/lexer/_YangLexer.java
+++ intelij-plugin/src/main/gen/tech/pantheon/yanginator/plugin/lexer/_YangLexer.java
@@ -1,6 +1,6 @@
 /*
  *
- *   Copyright (c) 2021-2025 PANTHEON.tech, s.r.o. All rights reserved.
+ *   Copyright (c) 2021-2026 PANTHEON.tech, s.r.o. All rights reserved.
  *
  *   This program and the accompanying materials are made available under the
  *   terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -3245,7 +3245,7 @@ public class _YangLexer implements FlexLexer {
 
     /* user code: */
     public _YangLexer() {
-        this((java.io.Reader) null);
+        this(null);
     }
 
 
