230f915517613806b83554721b3566e5918b80af
Lubos <lubos.janoscik@pantheon.tech>
2026-03-24 09:46:47 +0100

    Multiple line issue (#303) New feature -> press ENTER -> Quotes new line / Regex/Pattern '+ ""'
    Signed-off-by: Lubos <lubos.janoscik@pantheon.tech>

--- intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/injection/RegExpToYangInjector.java
+++ intelij-plugin/src/main/java/tech/pantheon/yanginator/plugin/injection/RegExpToYangInjector.java
@@ -18,6 +18,7 @@ import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.XsdRegExpParserDefinition;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
+import tech.pantheon.yanginator.plugin.psi.YangIndentableQuotedString;
 import tech.pantheon.yanginator.plugin.psi.YangPatternBody;
 import tech.pantheon.yanginator.plugin.psi.YangQuotedString;
 import tech.pantheon.yanginator.plugin.psi.YangStringSplitter;
@@ -27,28 +28,20 @@ import java.util.List;
 public class RegExpToYangInjector implements MultiHostInjector {
     @Override
     public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
-        // 1. Handle multiple line string (with plus at the start/end of the quotes)
-        if (context instanceof YangStringSplitter) {
-            List<YangQuotedString> parts = PsiTreeUtil.getChildrenOfTypeAsList(context, YangQuotedString.class);
+        if (!(context instanceof YangIndentableQuotedString)) return;
+        if (!(context.getParent() instanceof YangPatternBody)) return;
 
-            if (!parts.isEmpty()) {
-                registrar.startInjecting(XsdRegExpParserDefinition.LANGUAGE);
-                for (YangQuotedString part : parts) {
-                    registrar.addPlace(null, null, part,
-                            new TextRange(1, part.getTextLength() - 1));
-                }
-                registrar.doneInjecting();
+        List<YangQuotedString> quoteParts = PsiTreeUtil.getChildrenOfTypeAsList(context, YangQuotedString.class);
+
+        if (!quoteParts.isEmpty()) {
+            registrar.startInjecting(XsdRegExpParserDefinition.LANGUAGE);
+
+            for (YangQuotedString part : quoteParts) {
+                registrar.addPlace(null, null, (PsiLanguageInjectionHost) part,
+                        new TextRange(1, part.getTextLength() - 1));
             }
-            return;
-        }
 
-        // 2. Handle Single Strings
-        if (context instanceof YangQuotedString &&
-                context.getParent() instanceof YangPatternBody) {
-            registrar.startInjecting(XsdRegExpParserDefinition.LANGUAGE)
-                    .addPlace(null, null, (PsiLanguageInjectionHost) context,
-                            new TextRange(1, context.getTextLength() - 1))
-                    .doneInjecting();
+            registrar.doneInjecting();
         }
     }
 
